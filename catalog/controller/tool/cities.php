fc2f6c0d0b73f43ff33c42481bbd7b040c7393b1
sitesoft1 <sitesoft1@gmail.com>
2020-10-24 04:24:48 +0300

    Город в зависимости от области

---
+++ catalog/controller/tool/cities.php
@@ -0,0 +1,6 @@
+<?php
+class ControllerToolCities extends Controller {
+	public function index() {
+		echo 'test get cities by ajax!';
+	}
+}
--- catalog/model/tool/deliverygeo.php
+++ catalog/model/tool/deliverygeo.php
@@ -7,7 +7,8 @@
 class ModelToolDeliveryGeo extends Model {
    
     public function getCdekRegions() {
-        $query = $this->db->query("SELECT * FROM ".DB_PREFIX."ll_cdek_region WHERE `country_id`='1' AND `id`>1");
+        //$query = $this->db->query("SELECT * FROM ".DB_PREFIX."ll_cdek_region WHERE `country_id`='1' AND `id`>1");
+        $query = $this->db->query("SELECT * FROM ".DB_PREFIX."ll_cdek_region WHERE `country_id`='1'");
         if ($query->num_rows) {
             return $query->rows;
         }
